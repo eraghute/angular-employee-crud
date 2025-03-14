eb9675c673a87d1fd49057b9e3ce7167ce580d42
Esha Raghute <eraghute@gmail.com>
2025-03-14 11:15:05 +0000

    datepicker added

--- src/app/manage-employee/manage-employee.component.ts
+++ src/app/manage-employee/manage-employee.component.ts
@@ -4,18 +4,24 @@ import {MatDialogModule, MatDialogRef} from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import {MatRadioModule} from '@angular/material/radio';
+import {MatCalendarCellClassFunction, MatDatepickerModule} from '@angular/material/datepicker';
+import { provideNativeDateAdapter } from '@angular/material/core';
 
 @Component({
   selector: 'app-manage-employee',
-  imports: [MatDialogModule, MatInputModule, MatButtonModule, MatFormFieldModule,MatRadioModule],
+  imports: [MatDialogModule, MatInputModule, MatButtonModule, MatDatepickerModule, MatFormFieldModule,MatRadioModule],
   templateUrl: './manage-employee.component.html',
-  styleUrl: './manage-employee.component.scss'
+  styleUrl: './manage-employee.component.scss',
+  providers: [provideNativeDateAdapter()],
 })
 export class ManageEmployeeComponent {
   readonly dialogRef = inject(MatDialogRef<ManageEmployeeComponent>);
-  constructor(private _dialog: MatDialogModule) {}
+  dateClass: MatCalendarCellClassFunction<Date> = (cellDate, view) => {
+    return '';
+  };
+  constructor() {}
 
-  onNoClick() {
+  cancel() {
     this.dialogRef.close();
   }
 }
